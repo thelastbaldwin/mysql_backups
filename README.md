f9349e82ee101885761020395a935e830bf75e8c
Steve Minor <stv.mnr@gmail.com>
2017-03-07 19:07:20 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+MySQL database backup script
+============================
+This script uses mysqldump to create database backups in a given directory.
+In order to use it, you'll need to edit the script with your database credentials 
+and set up a cron job so that the script runs automatically. 
+
+You can specify within the script where the backups should be stored and how many 
+backups to store. 
+
+I recommend setting the permissions of the file to 750 and creating a special 
+database username and password for this file. The user should only have the permissions
+necessary to copy the database tables. 
+
+To have the script run daily at midnight, execute:
+  crontab -eu <USER>
+And enter:
+  0 0 * * * /path_to_script/db_backup.sh 
---
+++ db_backup.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Name: db_backup.sh
+# Author: Steve Minor
+# Date: 03/07/2017
+# Description: Creates a rotating set of DB backups with mysqldump and crontab
+
+# Replace these variables
+DB_USER=root
+DB_PASS=root
+DB_NAME=sminor_wordpress
+NUM_BACKUPS=10
+BACKUP_DIR=./backups
+
+NOW="$(date +%m_%d_%Y+%H:%M:%S)"
+
+# Create the backup
+echo "Creating backup: $DB_NAME-$NOW.sql"
+mysqldump $DB_NAME -p$DB_PASS -u$DB_USER --add-drop-table> "./backups/$DB_NAME-$NOW.sql"
+
+# Remove all but the most recent backups, depending on number
+cd $BACKUP_DIR
+rm `ls -t | awk -v awk_num_backups=$NUM_BACKUPS 'NR>awk_num_backups'`
+
+# Edit the user cron jobs with the following
+# crontab -eu <USER>
+# To have the task run daily at midnight, enter
+# 0 0 * * * /path_to_script/db_backup.sh 
